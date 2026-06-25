d110caa5867678d98cd39a143734e9c5b923ee86
Xyon <pj.kostera@gmail.com>
2026-06-26 00:04:26 +0200

    added database schema (and data to my database)

--- models/shopItems.ts
+++ models/shopItems.ts
@@ -1,29 +1,67 @@
 import {Schema, models, model} from "mongoose";
 
-interface Item {
-    itemID: number,
+interface Car {
+    carID: number,
     name: string,
     price: number,
     src: string[],
     miniDescription: string,
-    description: string,
     sale: boolean,
     salePercent: number,
     available: boolean,
     createdAt?: Date
 }
 
-const ItemSchema = new Schema<Item>({
-    itemID: {type: Number, required: true, unique: true},
+interface CarInfo{
+    CarInfoID: number,
+    brand: string,
+    model: string,
+    mileage: number,
+    fuel: string,
+    gearbox: string,
+    type: string,
+    engine: number,
+    hp: number,
+    driveType: string,
+    color: string,
+    doorNum: number,
+    sitsNum: number,
+    description: string
+}
+
+const CarSchema = new Schema<Car>({
+    carID: {type: Number, required: true, unique: true},
     name: {type: String, required: true},
     price: {type: Number, required: true},
     src: {type: [String], required: true},
     miniDescription: {type: String, required: true},
-    description: {type: String, required: true},
     sale: {type: Boolean, required: true, default: false},
     salePercent: {type: Number, default: 0, min: 0, max: 100},
     available: {type:Boolean, default: true},
     createdAt: {type: Date, default: Date.now}
 })
 
-export default models.Item || model<Item>("Item", ItemSchema);
+const CarInfoSchema = new Schema<CarInfo>({
+    CarInfoID: {type: Number, required: true, unique: true},
+    brand: {type: String, required: true},
+    model: {type: String,  required: true},
+    mileage: {type: Number,  required: true},
+    fuel: {type: String, required: true},
+    gearbox: {type: String, required: true},
+    type: {type: String, required: true},
+    engine: {type: Number, required: true},
+    hp: {type: Number, required: true},
+    driveType: {type: String, required: true},
+    color: {type: String, required: true},
+    doorNum: {type: Number, required: true},
+    sitsNum: {type: Number, required: true},
+    description: {type: String, required: true},
+  },
+  {
+    _id: false,
+  }
+);
+
+const CarModel = models.Car || model<Car>("Car", CarSchema);
+
+export default CarModel;
